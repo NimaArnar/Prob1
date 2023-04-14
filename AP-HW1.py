ee89499db96eb1e02bdfbd47f331cd568bc8f77a
NimaArnar <129725571+NimaArnar@users.noreply.github.com>
2023-04-14 06:16:32 +0330

    Mammals Class Added

--- AP-HW1.py
+++ AP-HW1.py
@@ -18,3 +18,33 @@ class VertebratesAnimal:
 
 v = VertebratesAnimal(15,"male","food","odise","place")
 print(v.name)
+
+class Mammals(VertebratesAnimal):
+    def __init__(self, age, gender, diet, name, habitat, species, covering, lifespan, fourchamberedheart, hibernate: bool = True):
+        super().__init__(age, gender, diet, name, habitat)
+        self.species = species
+        self.covering = covering
+        self.lifespan = lifespan
+        self.fourchamberedheart = fourchamberedheart
+        self.Hibernate = hibernate
+    def move(self):
+        return f"{self.name} is moving"
+    def swim(self):
+        return f"{self.name} can swim"
+    def communicate(self):
+        return f"{self.name} can communicate"
+    def hibernate(self):
+        if self.hibernate == True:
+            return f"{self.name} can hibernate"
+        else:
+            return f"{self.name} can not hibernate"
+    def description(self):
+        return f"{self.name} is {self.species} mammals with {self.covering} body and {self.age} years old "
+
+m = Mammals(23, "male", "meat", "Beeeear", "snow", "polar bear", "hair", 15, "Yes",False)
+print(m.swim())
+print(m.covering)
+print(m.hibernate())
+print(m.description())
+print(m.move())
+print(m.communicate())
